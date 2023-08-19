b3660cf5143427570dd824a2e817b8ce05670703
amal sami <amalsamii13@gmail.com>
2023-08-19 22:31:08 +0300

    render home page data

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,14 +1,20 @@
-function Card() {
+function Card({ movie }) {
+  console.log(movie);
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          <img src="" alt="" />
-          <h4>title</h4>
+
+    <div className="col-md-2 position-relative ">
+      <div className="item position-relative overflow-hidden">
+        <img src={"https://image.tmdb.org/t/p/w500" +
+          movie.poster_path
+        } className="w-100" alt="" />
+        <div className="overlay p-2 text-center">
+          {movie.overview.split(" ").splice(0, 18).join(" ")}...
         </div>
       </div>
-
+      <h4>{movie.title} {movie.name}</h4>
+      <div className="vote bg-info p-2 position-absolute top-0 end-0 ">{movie.vote_average.toFixed(1)}</div>
     </div>
+
   )
 }
 
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,42 @@
 import Card from "../Card/Card.jsx";
+import axios from "axios"
+import { useEffect, useState } from "react";
+
+
 function Home() {
+  const [movies, setMovies] = useState([])
+  const [tv, setTv] = useState([])
+  async function getTrending(type, setdata) {
+    const data = await axios.get(`https://api.themoviedb.org/3/trending/${type}/day?api_key=62b0fc4666a7121de866ed1aa6ce36d7`)
+
+    setdata(data.data.results)
+    console.log(data.data.results);
+  }
+
+  useEffect(() => {
+    getTrending("movie", setMovies)
+    getTrending("tv", setTv)
+  })
   return (
-    <div>
-      home
-      <Card></Card>
+    <div className="container">
+      <div className="row">
+        <div className="col-4">
+          <div className=" content d-flex justify-content-center h-100 flex-column">
+            <h2 className=" position-relative"> Trending <br></br> movies <br></br> to watch</h2>
+            <p className="">Watch them now on our app</p>
+          </div>
+        </div>
+        {movies?.slice(0, 10).map((movie) => <Card movie={movie}></Card>)}
+      </div>
+      <div className="row">
+        <div className="col-4">
+          <div className=" content d-flex justify-content-center h-100 flex-column">
+            <h2 className=" position-relative"> Trending <br></br> TV <br></br> to watch</h2>
+            <p className="">Watch them now on our app</p>
+          </div>
+        </div>
+        {tv?.slice(0, 10).map((tv) => <Card movie={tv}></Card>)}
+      </div>
     </div>
   );
 }
